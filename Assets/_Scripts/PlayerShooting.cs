424c78587823779afef88c729350473159e63203
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-11-20 23:24:44 -0500

    added impact effects

--- Assets/_Scripts/PlayerShooting.cs
+++ Assets/_Scripts/PlayerShooting.cs
@@ -16,12 +16,14 @@ public class PlayerShooting : MonoBehaviour
     public GameObject impact;
     public Animator rifleAnimator;
     public AudioSource bulletFireSound;
+    public AudioSource bulletImpactSound;
 
 
     // PRIVATE INSTANCE VARIABLES
     private GameObject[] _impacts;
     private int _currentImpact = 0;
     private int _maxImpacts = 5;
+    private Transform _transform;
 
     private bool _shooting = false; // gun idle is not shooting
 
@@ -29,6 +31,9 @@ public class PlayerShooting : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+
+        // reference to the gameObject's transform component
+        this._transform = gameObject.GetComponent<Transform>(); 
         // create an object pool - loop 5 objects
 	    this._impacts = new GameObject[this._maxImpacts];
 	    for (int impactCount = 0; impactCount < this._maxImpacts ; impactCount++)
@@ -53,4 +58,29 @@ public class PlayerShooting : MonoBehaviour
             this._shooting = false;
 	    }
 	}
+
+    // Physics Effects
+    void FixedUpdate()
+    {
+        if (this._shooting)
+        {
+            this._shooting = false;
+            RaycastHit hit;
+
+            Debug.DrawRay( this._transform.position, this._transform.forward );
+            if (Physics.Raycast (this._transform.position, this._transform.forward, out hit, 50f))
+            {
+                // show particle system at location of ray impact
+                this._impacts[this._currentImpact].transform.position = hit.point;
+                this._impacts[this._currentImpact].GetComponent<ParticleSystem>().Play();
+                this.bulletFireSound.Play();
+
+                // ensure array does not go out of bounds
+                if (++this._currentImpact >= this._maxImpacts)
+                {
+                    this._currentImpact = 0;
+                }
+            }
+        }
+    }
 }
